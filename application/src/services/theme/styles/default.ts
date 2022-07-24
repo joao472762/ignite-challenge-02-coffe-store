5e961849954d795e7bee8b57289421f1c61a28cf
João Victor de Souza Oliveira <tygasouzaa@gmail.com>
2022-07-24 08:33:32 -0300

    set the theme and fonts

---
+++ application/src/services/theme/styles/default.ts
@@ -0,0 +1,24 @@
+export const theme = {
+    colors: {
+        'white-100': '#FFF',
+        backgoround: '#FAFAFA',
+        Card:  '#F3F2F2',
+        Input: '#EDEDED',
+        Button:'#E6E5E5',
+        Hover: '#D7D5D5',
+        Label: '#8D8686',
+        Text:  '#574F4D',
+        SubTitle: '#403937',
+        Tittle: '#272221',
+
+        'purple-100': '#EBE5F9',
+        'purple-300': '#8047F8',
+        'purple-600': '#4B2995',
+
+        'yellow-100': '#F1E9C9',
+        'yellow-300': '#DBAC2C',
+        'yellow-600': '#DBAC2C'
+
+
+    }
+}
