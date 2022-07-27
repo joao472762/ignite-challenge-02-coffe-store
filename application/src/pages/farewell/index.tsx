56f5133065a96724860b92446719f97f557ba937
João Victor de Souza Oliveira <tygasouzaa@gmail.com>
2022-07-27 00:23:27 -0300

    firewell Component stylized

--- application/src/pages/farewell/index.tsx
+++ application/src/pages/farewell/index.tsx
@@ -1,27 +1,36 @@
 import {CurrencyDollar,Timer,MapPin} from 'phosphor-react'
 import IlustrationSvg from '../../assets/Illustration.svg'
-import { FarewellContainer } from './styles'
+import { theme } from '../../theme/styles/default'
+import { FarewellContainer, IconContainer, InformationsContaier } from './styles'
 export function Farewell(){
+    const {} = theme
     return (
         <FarewellContainer>
             <main>
                 <h1>Uhu! Pedido confirmado</h1>
                 <p>Agora é só aguardar que logo o café chegará até você</p>
-                <section>
+                <InformationsContaier>
                     <div>
-                        <MapPin/>
+                        <IconContainer>
+                            <MapPin weight='fill' size={40}/>
+                        </IconContainer>
                         <p>Entrega em <strong>Rua João Daniel Martinelli 102</strong>Farrapos - Porto Alegre, RS</p>
                     </div>
                     <div>
-                        <Timer/>
-                        <span>Previsão de entrega</span>
-                        <strong> 20 min - 30 min</strong>
+                        <Timer weight='fill' className='timer'/>
+                        <div>
+                            <span>Previsão de entrega</span>
+                            <strong> 20 min - 30 min</strong>
+                        </div>
                     </div>
                     <div>
-                        <span>Pagamento na entrega</span>
-                        <strong>Cartão de Crédito</strong>
+                        <CurrencyDollar className='currency'/>
+                        <div>
+                            <span>Pagamento na entrega</span>
+                            <strong>Cartão de Crédito</strong>
+                        </div>
                     </div>
-                </section>
+                </InformationsContaier>
             </main>
             <aside>
                 <img src={IlustrationSvg} />
--- application/src/pages/farewell/styles.ts
+++ application/src/pages/farewell/styles.ts
@@ -1,3 +1,81 @@
 import styled from "styled-components";
+interface iconBackground {
+    variant: 'yellow-600' | 'yellow-300'
+}
+export const FarewellContainer = styled.div`
+    display: flex;
+    column-gap: 6.375rem;
 
-export const FarewellContainer = styled.div``
+    margin-top: 7rem;
+
+    main{
+        flex: 1;
+
+        h1{
+            font-size: 2rem;
+            font-family: ${props => props.theme.fonts.Ballo};;
+            color: ${props => props.theme.colors["yellow-600"]};
+            font-weight: 800;
+            margin-bottom: .25rem;
+         
+        }
+        p{
+            font-size: 1.25rem;
+            color: ${props => props.theme.colors.SubTitle};
+        }
+    }
+`
+export const InformationsContaier = styled.section`
+    display: flex;
+    flex-direction: column;
+
+    padding: 2.25rem;
+    margin-top: 2.25rem;
+    border-radius: 6px 36px 6px 36px;
+    border: 1px solid ${props => props.theme.colors["purple-300"]};
+    row-gap: 2rem;
+
+  
+    div{
+        display: flex;
+        column-gap: 0.75rem;
+        color: ${props => props.theme.colors.Text};
+
+        p{
+            font-size:  1rem;
+            
+        }
+
+        
+        svg{
+            background-color: ${props => props.theme.colors["purple-300"]};
+            width: 2rem;
+            padding: .4rem;
+            height: 2rem;
+            color: ${props => props.theme.colors["white-100"]};
+            border-radius: 15px;
+
+            &.timer{
+                background-color: ${props => props.theme.colors["yellow-300"]};
+            }
+            &.currency{
+                background-color: ${props => props.theme.colors["yellow-600"]};
+            }
+        }
+
+      
+    };
+`
+export const IconContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+
+    height: 2rem;
+    width: 2rem;
+    background-color: ${props =>props.theme.colors["purple-300"]};
+    border-radius: 2rem;
+
+ 
+`
